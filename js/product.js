a1127ecb34a607a78707a6e7e676c7fd44001974
Riffat Akash <riffatakash@gmail.com>
2021-05-18 18:36:35 +0600

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,38 @@
+var products = [
+        {
+            "id":"1",
+            "title":"Lifestyle",
+            "link":"https://www.grameenphone.com/lifestyle-carnival",
+            "image_link":"1.jpg",
+        },
+        {
+            "id":"2",
+            "title":"Bogo",
+            "link":"https://www.grameenphone.com/food-carnival",
+            "image_link":"2.jpg",
+        },
+        {
+            "id":"3",
+            "title":"Food",
+            "link":"https://www.grameenphone.com/food-carnival",
+            "image_link":"3.jpg",
+        },
+        // {
+        //     "id":"4",
+        //     "title":"Furniture",
+        //     "link":"https://www.grameenphone.com/furniture-carnival",
+        //     "image_link":"furniture.jpg",
+        // },
+        // {
+        //     "id":"5",
+        //     "title":"Electronics",
+        //     "link":"https://www.grameenphone.com/electronics-carnival",
+        //     "image_link":"electronics.jpg",
+        // },
+        // {
+        //     "id":"6",
+        //     "title":"Grocery",
+        //     "link":"https://www.grameenphone.com/grocery-carnival",
+        //     "image_link":"grocery.jpg",
+        // },
+    ];
